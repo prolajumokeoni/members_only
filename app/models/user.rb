06f2b6a2f17b6bf6384b042a7ac2f266b0edcfd0
prolajumokeoni <prolajumokeoni@gmail.com>
2021-07-02 16:52:55 +0100

    Add readme and fix errors

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,10 +15,9 @@ class User < ApplicationRecord
   def email_changed?
     false
   end
-  
+
   # use this instead of email_changed? for Rails = 5.1.x
   def will_save_change_to_email?
     false
   end
-  
 end
