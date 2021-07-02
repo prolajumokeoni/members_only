8e671100852a4318ae137bde043eedbd020f3522
prolajumokeoni <prolajumokeoni@gmail.com>
2021-07-02 12:17:19 +0100

    Add CRUD functions to post and create comment

--- db/schema.rb
+++ db/schema.rb
@@ -15,8 +15,10 @@ ActiveRecord::Schema.define(version: 2021_07_02_104611) do
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "body"
+    t.integer "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -31,4 +33,5 @@ ActiveRecord::Schema.define(version: 2021_07_02_104611) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "posts", "users"
 end
