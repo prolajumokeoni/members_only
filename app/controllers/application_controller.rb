845ffc4e2a6fc5ec5e07aec16ddd352929e78033
prolajumokeoni <prolajumokeoni@gmail.com>
2021-07-04 04:17:06 +0100

    Fix errors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception  
+  protect_from_forgery with: :exception
 
   before_action :configure_permitted_parameters, if: :devise_controller?
 
@@ -7,10 +7,10 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up,
-      keys: [ :username, :email, :password, :password_confirmation])
+                                      keys: %i[username email password password_confirmation])
     devise_parameter_sanitizer.permit(:sign_in,
-      keys: [ :login, :password, :password_confirmation])
+                                      keys: %i[login password password_confirmation])
     devise_parameter_sanitizer.permit(:account_update,
-      keys:[ :username, :email, :password_confirmation, :current_password ])
-    end
+                                      keys: %i[username email password_confirmation current_password])
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,16 +1,17 @@
 class User < ApplicationRecord
   attr_accessor :login
-  
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
   has_many :posts
 
-  def self.find_for_database_authentication warden_condition
-    conditions = warden_condition.dup 
+  def self.find_for_database_authentication(warden_condition)
+    conditions = warden_condition.dup
     login = conditions.delete(:login)
     where(conditions).where(
-      ["lower(username) = :value OR lower(email) = :value", 
-      {value: login.strip.downcase}]).first
+      ['lower(username) = :value OR lower(email) = :value',
+       { value: login.strip.downcase }]
+    ).first
   end
 end
